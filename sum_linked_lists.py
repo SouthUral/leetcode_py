e2ae58519df90a6a4474b050fcdd9eb3892ee1cb
vladimir <vladimir1012kov@gmail.com>
2022-10-04 12:09:37 +0300

    first commit

---
+++ sum_linked_lists.py
@@ -0,0 +1,15 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def unpacking_linked_list(self, link_list):
+        res = []
+        res.append(link_list.val)
+        next = link_list.next
+        
+    
+    
+    def addTwoNumbers(self, l1, l2):
